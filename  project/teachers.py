2c8a85b2a7b8f7696ec111dbb4161b69348f356b
mayagro2005 <120019695+mayagro2005@users.noreply.github.com>
2023-01-04 20:19:22 +0200

    Add files via upload

---
+++  project/teachers.py
@@ -0,0 +1,237 @@
+import sqlite3
+import hashlib
+
+class teachers(object):
+    def __init__(self, tablename = "teachers", teacherId = "teacherId", firstname = "firstname", lastname = "lastname", email = "email", password = "password"):
+        self.__tablename = tablename
+        self.__teacherId = teacherId
+        self.__firstname = firstname
+        self.__lastname = lastname
+        self.__email = email
+        self.__password = password
+
+        conn = sqlite3.connect('test.db')
+        print ("Opened database successfully")
+        str = "CREATE TABLE IF NOT EXISTS " + self.__tablename + "(" + self.__teacherId + " " + "INTEGER PRIMARY KEY AUTOINCREMENT ,"
+        str += " " + self.__firstname + " TEXT    NOT NULL ,"
+        str += " " + self.__lastname + " TEXT    NOT NULL, "
+        str += " " + self.__email + " TEXT    NOT NULL, "
+        str += " " + self.__password + " TEXT    NOT NULL) "
+        conn.execute(str)
+        print("Table created successfully");
+        conn.commit()
+        conn.close()
+    def get_table_name(self):
+        return self.__tablename
+    def get_all_teachers(self):
+        try:
+            conn = sqlite3.connect('test.db')
+            print("Opened database successfully");
+            str1 = "select*from " + self.__tablename
+            print(str1)
+            cursor = conn.execute(str1)
+            rows = cursor.fetchall()
+            arr_teachers = []
+            for row in rows:
+                str_rows = str(row[0]) + " " + str(row[1]) + " " + str(row[2]) + " " + str(row[3]) + " " + str(row[4])
+                arr_teachers.append(str_rows)
+            print(arr_teachers)
+            return arr_teachers
+        except:
+            return False
+    def insert_teacher(self, firstname, lastname, email, password):
+        try:
+            conn = sqlite3.connect('test.db')
+            # salt = 'GROSSMAN'
+            # salt_password = hashlib.md5(salt.encode('utf-8') + password.encode('utf-8')).hexdigest()
+            # print(salt_password)
+            str_insert = "INSERT INTO " + self.__tablename + " (" + self.__firstname + "," + self.__lastname + "," + self.__email + "," + self.__password +\
+                         ") VALUES (" + "'" + firstname + "'" + "," + "'" + lastname + "'" + "," + "'" + email + "'" + "," + "'" + password + "');"
+            print(str_insert)
+            conn.execute(str_insert)
+            conn.commit()
+            conn.close()
+            print("Record created successfully");
+            return True
+        except:
+            print("Failed to insert user")
+            return False
+    def delete_teacher(self, firstname, lastname, email, password):
+        try:
+            conn = sqlite3.connect('test.db')
+            str_delete = "DELETE  from " + self.__tablename + " where " + self.__firstname + "=" + "'"+str(firstname)+"'" +\
+                         ' and ' + self.__lastname + "=" + "'"+str(lastname)+ "'" + ' and ' + self.__email + "=" + "'"+str(email)+ "'" + ' and ' + self.__password + "=" + "'"+str(password) + "'"
+            print(str_delete)
+            conn.execute(str_delete)
+            conn.commit()
+            conn.close()
+            print("Record deleted successfully")
+            return "Success"
+        except:
+            return "Failed to delete user"
+
+    def is_exist(self, email, password):
+        conn = sqlite3.connect('test.db')
+        print("Opened database successfully")
+        salt = 'GROSSMAN'
+        salt_password = hashlib.md5(salt.encode('utf-8') + password.encode('utf-8')).hexdigest()
+
+        str_check = f"SELECT * from {self.__tablename} where '{email}' = {self.__email} and " \
+                    f"'{salt_password}' = {self.__password}"
+        print(str_check)
+        cursor = conn.execute(str_check)
+        row = cursor.fetchall()
+        if row:
+            print("Exist")
+            return True
+        else:
+            print("Not exist")
+            return False
+
+    def update_teacher(self, teacherId, firstname, lastname, email, password):
+        try:
+            conn = sqlite3.connect('test.db')
+            str_update = " UPDATE " + self.__tablename + " set "
+            str_update += self.__firstname + "=" + "'" + firstname + "',"
+            str_update += self.__lastname + "=" + "'" + lastname + "',"
+            str_update += self.__email + "=" + "'" + email + "',"
+            str_update += self.__password + "=" + "'" + password + "'"
+            str_update += " WHERE " + self.__teacherId + "=" + "'" + str(teacherId) + "'"
+            print(str_update)
+            conn.execute(str_update)
+            conn.commit()
+            conn.close()
+            print("teacher updated successfully")
+            return "Success"
+        except:
+            print("error")
+            return "Failed to update user by id"
+
+
+    def get_teacher_by_id(self, teacherId):
+        try:
+            conn = sqlite3.connect('test.db')
+            print("Opened database successfully")
+            # salt = 'GROSSMAN'
+            # salt_password = hashlib.md5(salt.encode('utf-8') + password.encode('utf-8')).hexdigest()
+            # print(salt_password)
+            # strsql= f"SELECT * from {self.__tablename} where '{self.__email}' = {str(email)} and {self.__password}" \
+            #             f" = '{str(salt_password)}"
+            # print(strsql)
+
+            strsql = "SELECT * from " + self.__tablename + " where " + self.__teacherId + "=" + "'" + str(teacherId) + "'"
+            print(strsql)
+            cursor = conn.execute(strsql)
+            row = cursor.fetchone()
+            arr_teachers = []
+            if row:
+                str_rows = str(row[0]) + " " + str(row[1]) + " " + str(row[2]) + " " + str(row[3]) + " " + str(row[4])
+                # return [row[0], row[1], row[2], row[3], row[4]]
+                arr_teachers.append(str_rows)
+                print(arr_teachers)
+
+                return arr_teachers
+                # return [row[0], row[1], row[2], row[3], row[4]]
+
+            else:
+                print("Failed to find user")
+                return False
+            conn.commit()
+            conn.close()
+        except:
+            return False
+
+
+    def get_teacher_by_email(self, email):
+        try:
+            conn = sqlite3.connect('test.db')
+            print("Opened database successfully")
+            # salt = 'GROSSMAN'
+            # salt_password = hashlib.md5(salt.encode('utf-8') + password.encode('utf-8')).hexdigest()
+            # print(salt_password)
+            # strsql= f"SELECT * from {self.__tablename} where '{self.__email}' = {str(email)} and {self.__password}" \
+            #             f" = '{str(salt_password)}"
+            # print(strsql)
+
+            strsql = "SELECT * from " + self.__tablename + " where " + self.__email + "=" + "'" + str(email) + "'"
+            print(strsql)
+            cursor = conn.execute(strsql)
+            row = cursor.fetchone()
+            arr_teachers = []
+            if row:
+                str_rows = str(row[0]) + " " + str(row[1]) + " " + str(row[2]) + " " + str(row[3]) + " " + str(row[4])
+                # return [row[0], row[1], row[2], row[3], row[4]]
+                arr_teachers.append(str_rows)
+                print(arr_teachers)
+
+                return arr_teachers
+
+            else:
+                print("Failed to find user")
+                return False
+            conn.commit()
+            conn.close()
+        except:
+            return False
+
+
+    def get_teacher_by_email_and_password(self, email, password):
+        try:
+            conn = sqlite3.connect('test.db')
+            print("Opened database successfully")
+            # salt = 'GROSSMAN'
+            # salt_password = hashlib.md5(salt.encode('utf-8') + password.encode('utf-8')).hexdigest()
+            # print(salt_password)
+            # strsql= f"SELECT * from {self.__tablename} where '{self.__email}' = {str(email)} and {self.__password}" \
+            #             f" = '{str(salt_password)}"
+            # print(strsql)
+
+            strsql = "SELECT * from " + self.__tablename + " where " + self.__email + "=" + "'" + str(email) + "'" + ' and ' + self.__password + "=" + "'" + str(password) + "'"
+            print(strsql)
+            cursor = conn.execute(strsql)
+            row = cursor.fetchone()
+            arr_teachers = []
+            if row:
+                str_rows = str(row[0]) + " " + str(row[1]) + " " + str(row[2]) + " " + str(row[3]) + " " + str(row[4])
+                # return [row[0], row[1], row[2], row[3], row[4]]
+                arr_teachers.append(str_rows)
+                print(arr_teachers)
+
+                return arr_teachers
+
+            else:
+                print("Failed to find user")
+                return False
+            conn.commit()
+            conn.close()
+        except:
+            return False
+
+    def __str__(self):
+        return "table  name is ", self.__tablename
+
+
+t=teachers()
+# t.insert_teacher("Maya", "grossman", "we12.com", 'fgh1234')
+# t.insert_teacher("gaya", "tyu", "re34.com", '1234')
+# t.insert_teacher("anna", "sdxcf", "dgfcgv123.com", 'qwr567')
+# t.insert_teacher("xcgvb", "zxcv", "d3.com", 'q7')
+# t.get_teacher_by_id('4')
+# t.delete_teacher("Maya", "grossman", "we12.com", 'fgh1234')
+# t.get_teacher_by_email("d3.com")
+# t.get_all_teachers()
+# t.update_teacher('6', "lilya", "qwew", "qwe56", 'zxc567')
+# t.get_teacher_by_email_and_password("dgfcgv123.com", 'qwr567')
+
+
+
+
+#u.insert_user("v@y.com", "dvidi", 'davidi')
+#u.insert_user("t@a.com", "aba", 'origin')
+#u.select_all_users()
+#u.check_user_by_username("Asaf")
+#u.delete_username("Asaf")
+#u.check_user_by_username("Asaf")
+#user=u.return_user_by_email('u@x.com')
+#print(user[0])
+#print(user)
